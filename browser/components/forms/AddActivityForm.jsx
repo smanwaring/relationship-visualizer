7217ff5c03fee018e9abcad2b22c6f69093af055
Andrew Gionfriddo <andrew.gionfriddo@gmail.com>
2017-02-02 17:20:53 -0500

    when you click add activity it shows a menu, menu doesnt do anything yet

--- browser/components/forms/AddActivityForm.jsx
+++ browser/components/forms/AddActivityForm.jsx
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
+import DatePicker from 'material-ui/DatePicker';
 
 class AddActivityForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      type: ''
+    }
+    this.handleTypeChange = this.handleTypeChange.bind(this);
+  }
+
+  handleTypeChange(event, index, value) {
+    this.setState({type: value})
+  }
   
   render() {
     return (
       <div>
-        <h1>ADD FORM GOES HERE</h1>
+        <form>
+          <DropDownMenu value={this.state.type} onChange={this.handleTypeChange}>
+            <MenuItem value={''} primaryText="Type of Contact" />
+            <MenuItem value={'call'} primaryText="Call" />
+            <MenuItem value={'text'} primaryText="Text" />
+            <MenuItem value={'email'} primaryText="Email" />
+            <MenuItem value={'inPerson'} primaryText="In Person" />
+            <MenuItem value={'letter'} primaryText="Wrote Letter" />
+          </DropDownMenu>
+          <DatePicker hintText="Date" container="inline" />
+        </form>
       </div>
     )
   }
