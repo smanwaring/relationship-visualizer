19dd69e63b7220be5f50a0a902160b8e35289a06
Stephanie Manwaring <stephanie.manwaring@gmail.com>
2017-02-04 10:51:31 -0500

    fixed login order of operations

--- browser/utils/AuthService.js
+++ browser/utils/AuthService.js
@@ -1,80 +1,79 @@
 
-import { EventEmitter } from 'events'
-import Auth0Lock from 'auth0-lock'
-import { browserHistory } from 'react-router'
+import { EventEmitter } from 'events';
+import Auth0Lock from 'auth0-lock';
+import { browserHistory } from 'react-router'; 
 
 export default class AuthService extends EventEmitter {
   constructor(clientId, domain) {
-    super()
+    super();
     // Configure Auth0
     this.lock = new Auth0Lock(clientId, domain, {
       auth: {
         redirectUrl: 'http://localhost:1337/login',
         responseType: 'token'
       }
-    })
+    });
     // Add callback for lock `authenticated` event
-    this.lock.on('authenticated', this._doAuthentication.bind(this))
+    this.lock.on('authenticated', this._doAuthentication.bind(this));
     // Add callback for lock `authorization_error` event
-    this.lock.on('authorization_error', this._authorizationError.bind(this))
+    this.lock.on('authorization_error', this._authorizationError.bind(this));
     // binds login functions to keep this context
-    this.login = this.login.bind(this)
+    this.login = this.login.bind(this);
   }
 
   _doAuthentication(authResult) {
     // Saves the user token
-    this.setToken(authResult.idToken)
-    // navigate to the home route
-    browserHistory.replace('/')
+    this.setToken(authResult.idToken);
     // Async loads the user profile data
     this.lock.getProfile(authResult.idToken, (error, profile) => {
       if (error) {
-        console.log('Error loading the Profile', error)
+        console.log('Error loading the Profile', error);
       } else {
-        this.setProfile(profile)
+        this.setProfile(profile);
       }
-    })
+    });
   }
 
 
   _authorizationError(error){
-      if(error){
-          console.log(error)
+      if (error){
+          console.error(error);
       }
   }
-  // ...
 
   setProfile(profile) {
     // Saves profile data to local storage
-    localStorage.setItem('profile', JSON.stringify(profile))
+    localStorage.setItem('profile', JSON.stringify(profile));
     // Triggers profile_updated event to update the UI
-    this.emit('profile_updated', profile)
+    this.emit('profile_updated', profile);
+    // navigate to the home route
+    browserHistory.replace('/home');
   }
 
   getProfile() {
     // Retrieves the profile data from local storage
-    const profile = localStorage.getItem('profile')
-    return profile ? JSON.parse(localStorage.profile) : {}
+    const profile = localStorage.getItem('profile');
+    return profile ? JSON.parse(localStorage.profile) : {};
   }
-  
+
   login() {
     // Call the show method to display the widget.
-    this.lock.show()
+    this.lock.show();
   }
 
   loggedIn() {
     // Checks if there is a saved token and it's still valid
-    return !!this.getToken()
+    return !!this.getToken();
   }
 
   setToken(idToken) {
     // Saves user token to local storage
-    localStorage.setItem('id_token', idToken)
+    localStorage.setItem('id_token', idToken);
   }
 
   getToken() {
     // Retrieves the user token from local storage
-    return localStorage.getItem('id_token')
+    return localStorage.getItem('id_token');
   }
 
   logout() {
