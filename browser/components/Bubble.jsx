fc32c3e89cdca87797dd9d5f67759a3d5779c5b5
Andrew Gionfriddo <andrew.gionfriddo@gmail.com>
2017-02-08 10:48:12 -0500

    cleaned up the code and put the d3 code in its own separate file

--- browser/components/Bubble.jsx
+++ browser/components/Bubble.jsx
@@ -7,14 +7,8 @@ import Dialog from 'material-ui/Dialog';
 import * as d3 from "d3";
 import AddActivityForm from './forms/AddActivityForm';
 import { incrementScore } from '../reducers/relationships';
+import { animateBubbles } from '../d3/bubbleD3';
 
-let fakeData = {
-  "1": 10,
-  "2": 5,
-  "3": 7,
-  "4": 2,
-  "5": 15
-}
 
 class Bubble extends Component {
   constructor(props) {
@@ -22,63 +16,17 @@ class Bubble extends Component {
     this.state = {
       open: false
     }
-
-    this.handleOpen = () => {
-      this.setState({ open: true })
-    }
-
-    this.handleClose = () => {
-      this.setState({ open: false })
-    }
+    this.handleOpen = () => this.setState({ open: true }) 
+    this.handleClose = () => this.setState({ open: false });
     this.addToScore = this.addToScore.bind(this);
   }
 
   componentDidMount() {
-    
-    d3.select(`#score${this.props.relationship.id}`)
-      .transition()
-      .delay(2000)
-      .duration(500)
-      .attr("r", this.props.relationship.score * 4)
-      .attr("cx", this.props.relationship.score * 5)
-      .attr("cy", this.props.relationship.score * 5)
-
-
-    d3.select(`#score${this.props.relationship.id}-container`).transition()
-      .duration(2000)
-      .attr("width", this.props.relationship.score * 10)
-      .attr("height", this.props.relationship.score * 10)
-
-    d3.select(`#score${this.props.relationship.id}-name`).transition()
-      .delay(1000)
-      .duration(500)
-      .attr("x", this.props.relationship.score * 3)
-      .attr("y", this.props.relationship.score * 5)
-      .style("font-size", this.props.relationship.score * 2 + "px" )
+    animateBubbles(this.props.relationship);
   }
 
   componentDidUpdate() {
-
-    d3.select(`#score${this.props.relationship.id}-container`).transition()
-      .duration(1000)
-      .attr("width", this.props.relationship.score * 10)
-      .attr("height", this.props.relationship.score * 10)
-    
-    d3.select(`#score${this.props.relationship.id}`)
-      .transition()
-      .delay(1000)
-      .duration(500)
-      .attr("r", this.props.relationship.score * 4)
-      .attr("cx", this.props.relationship.score * 5)
-      .attr("cy", this.props.relationship.score * 5)
-
-    d3.select(`#score${this.props.relationship.id}-name`).transition()
-      .delay(1000)
-      .duration(500)
-      .attr("x", this.props.relationship.score * 3)
-      .attr("y", this.props.relationship.score * 5)
-      .style("font-size", this.props.relationship.score * 2 + "px" )
-
+    animateBubbles(this.props.relationship);
   }
 
   addToScore(relationship, user) {
@@ -103,17 +51,8 @@ class Bubble extends Component {
         <p>Type: {relationship.type}</p>
         <svg className="circle-container" id={`score${relationship.id}-container`}>
           <circle id={`score${relationship.id}`} style={{"fill": "steelblue"}}></circle>
-
-          {/*<text 
-            x={relationship.score / 2} 
-            y={relationship.score} 
-            style={{"fill": "black", "fontSize": relationship.score}}
-          >
-            {relationship.score}
-          </text>*/}
           <text id={`score${relationship.id}-name`} style={{"fill": "black" }}>{relationship.name}</text>
         </svg>
-
         <Link to={`/relationship/${relationship.id}/activities`}><RaisedButton label="View Activities" primary={true} /></Link>
         <RaisedButton label="Add Activity" secondary={true} onTouchTap={this.handleOpen} id="addActivity"/>
           <Dialog
---
+++ browser/d3/bubbleD3.js
@@ -0,0 +1,24 @@
+import * as d3 from "d3";
+
+export const animateBubbles = (relationship) => {
+      d3.select(`#score${relationship.id}`)
+      .transition()
+      .delay(1000)
+      .duration(500)
+      .attr("r", relationship.score * 4)
+      .attr("cx", relationship.score * 5)
+      .attr("cy", relationship.score * 5)
+
+
+    d3.select(`#score${relationship.id}-container`).transition()
+      .duration(1000)
+      .attr("width", relationship.score * 10)
+      .attr("height", relationship.score * 10)
+
+    d3.select(`#score${relationship.id}-name`).transition()
+      .delay(1000)
+      .duration(500)
+      .attr("x", relationship.score * 3)
+      .attr("y", relationship.score * 5)
+      .style("font-size", relationship.score * 2 + "px" )
+}
