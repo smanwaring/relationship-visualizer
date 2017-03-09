cf35d50b643b8acc011925a497c445df572ff1c4
Andrew Gionfriddo <andrew.gionfriddo@gmail.com>
2017-03-09 17:36:04 -0500

    background looks dope

--- browser/components/root.js
+++ browser/components/root.js
@@ -1,7 +1,20 @@
 import React, { PropTypes as T } from 'react';
 // import styles from './styles.module.css'
+import { fetchImage } from '../utils/FetchBackgroundImage';
+import axios from 'axios';
+
+const height = window.screen.availHeight;
+const width = window.screen.availWidth;
+
 
 class Root extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      image: ''
+    }
+  }
+
   render() {
     let children = null;
     if (this.props.children) {
@@ -10,10 +23,30 @@ class Root extends React.Component {
       });
     }
     return (
-      <div>
-        {children}
+      <div style={style.image}>
+        <div style={style.layer}>
+          {children}
+        </div>
       </div>
     );
+  }
+}
+
+const style = { 
+  image: {
+    backgroundImage: `url(https://unsplash.it/${width}/${height}/?random)`, 
+    width: '100%',
+    height: '100%'
+  },
+  layer: {
+    zIndex: '1',
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
+    position: 'fixed',
+    overflow: 'auto',
+    top: '0',
+    left: '0',
+    width: '100%',
+    height: '100%'
   }
 }
 
